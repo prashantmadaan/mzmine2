5171d393e7b09ee777fe1817ec95c49837e0673b
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-03-27 22:06:48 -0400

    Fix minor bugs in Significance and Mummichog

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/dataanalysis/mummichog/MummichogTask.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/dataanalysis/mummichog/MummichogTask.java
@@ -40,112 +40,120 @@ import pojo.Compound;
 
 public class MummichogTask extends AbstractTask {
 
-	private static final String SIGNIFICANCE_KEY = "SIGNIFICANCE";
-	private static final String P_VALUE_KEY = "P_Value";
-	private static final String T_VALUE_KEY = "T_Value";
+    private static final String SIGNIFICANCE_KEY = "SIGNIFICANCE";
+    private static final String P_VALUE_KEY = "P_Value";
+    private static final String T_VALUE_KEY = "T_Value";
 
-	private Logger logger = Logger.getLogger(this.getClass().getName());
-	private double finishedPercentage = 0.0;
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private double finishedPercentage = 0.0;
 
-	private final PeakListRow[] peakListRows;
-	private final String cutoff;
-	private final String network;
-	private final String force_primary_ion;
-	private final String modeling;
-//	private final String mode;
-	private final File output;
+    private final PeakListRow[] peakListRows;
+    private final String cutoff;
+    private final String network;
+    private final String force_primary_ion;
+    private final String modeling;
+    //	private final String mode;
+    private final File output;
 
-	public String getTaskDescription() {
-		return "Calculating Mummichog... ";
-	}
+    public String getTaskDescription() {
+        return "Calculating Mummichog... ";
+    }
 
-	public MummichogTask(PeakListRow[] peakListRows, String cutoff, String network, String force_primary_ion,
-			String modeling, File output) {
-		super();
-		this.peakListRows = peakListRows;
-		this.cutoff = cutoff;
-		this.network = network;
-		this.force_primary_ion = force_primary_ion;
-		this.modeling = modeling;
-	//	this.mode = mode;
-		this.output = output;
-	}
+    public MummichogTask(PeakListRow[] peakListRows, String cutoff, String network, String force_primary_ion,
+                         String modeling, File output) {
+        super();
+        this.peakListRows = peakListRows;
+        this.cutoff = cutoff;
+        this.network = network;
+        this.force_primary_ion = force_primary_ion;
+        this.modeling = modeling;
+        //	this.mode = mode;
+        this.output = output;
+    }
 
-	public double getFinishedPercentage() {
-		return finishedPercentage;
-	}
+    public double getFinishedPercentage() {
+        return finishedPercentage;
+    }
 
-	@SuppressWarnings({ "unlikely-arg-type", "unlikely-arg-type" })
-	public void run() {
-		setStatus(TaskStatus.PROCESSING);
-		String errorMsg = null;
-		try {
-			String input = prepareData();
-			ExecuteMummiChog emc = new ExecuteMummiChog();
-			String[] arguments = new String[12];
+    @SuppressWarnings({"unlikely-arg-type", "unlikely-arg-type"})
+    public void run() {
+        setStatus(TaskStatus.PROCESSING);
+        String errorMsg = null;
+        try {
+            String input = prepareData();
+            ExecuteMummiChog emc = new ExecuteMummiChog();
+            String[] arguments = new String[10];
 
-			arguments[0] = "--cutoff";
-			arguments[1] = this.cutoff;
-			arguments[2] = "--network";
-			arguments[3] = this.network;
-			arguments[4] = "--force_primary_ion";
-			arguments[5] = this.force_primary_ion;
-			arguments[6] = "--modeling";
-			arguments[7] = this.modeling;
-			//arguments[8] = "--mode";
-			//arguments[9] = this.mode;
-			arguments[8] = "--output";
-			arguments[9] = this.output.getAbsolutePath();
+            arguments[0] = "--cutoff";
+            arguments[1] = this.cutoff;
+            arguments[2] = "--network";
+            arguments[3] = this.network;
+            arguments[4] = "--force_primary_ion";
+            arguments[5] = this.force_primary_ion;
+            arguments[6] = "--modeling";
+            arguments[7] = this.modeling;
+            //arguments[8] = "--mode";
+            //arguments[9] = this.mode;
+            arguments[8] = "--output";
+            arguments[9] = this.output.getAbsolutePath();
 
-			@SuppressWarnings("unused")
-			Map<String, List<Compound>> mummiOutput = emc.runMummiChog(input, arguments);
+            @SuppressWarnings("unused")
+            Map<String, List<Compound>> mummiOutput = emc.runMummiChog(input, arguments);
 
-			// Code to update the allign peak list
-			for (String mzr : mummiOutput.keySet()) {
+            // Code to update the allign peak list
+            for (String mzr : mummiOutput.keySet()) {
 
-				for (PeakListRow pr : peakListRows) {
-					if ((String.valueOf(pr.getAverageMZ()) + ";" + String.valueOf(pr.getAverageRT()))
-							.equalsIgnoreCase(mzr)) {
-						for (Compound compund : mummiOutput.get(mzr)) {
-							PeakIdentity pId = new SimplePeakIdentity(compund.getName(), compund.getFormula(), "", "",
-									"");
-							pr.addPeakIdentity(pId, true);
-						}
-					}
-				}
-			}
+                for (PeakListRow pr : peakListRows) {
+                    if ((String.valueOf(pr.getAverageMZ()) + ";" + String.valueOf(pr.getAverageRT()))
+                            .equalsIgnoreCase(mzr)) {
+                        for (Compound compund : mummiOutput.get(mzr)) {
+                            PeakIdentity pId = new SimplePeakIdentity(compund.getName(), compund.getFormula(), "", "",
+                                    "");
+                            pr.addPeakIdentity(pId, true);
+                        }
+                    }
+                }
+            }
 
-			setStatus(TaskStatus.FINISHED);
-			logger.info("Calculating Mummichog is completed");
-		} catch (Exception e) {
-			errorMsg = "'Unknown Error' during Mummichog calculation: " + e.getMessage();
-		} catch (Throwable t) {
-			setStatus(TaskStatus.ERROR);
-			setErrorMessage(t.getMessage());
-			logger.log(Level.SEVERE, "Mummichog calculation error", t);
-		}
+            setStatus(TaskStatus.FINISHED);
+            logger.info("Calculating Mummichog is completed");
+        } catch (Exception e) {
+            errorMsg = "'Unknown Error' during Mummichog calculation: " + e.getMessage();
+        } catch (Throwable t) {
+            setStatus(TaskStatus.ERROR);
+            setErrorMessage(t.getMessage());
+            logger.log(Level.SEVERE, "Mummichog calculation error", t);
+        }
 
-		if (errorMsg != null) {
-			setErrorMessage(errorMsg);
-			setStatus(TaskStatus.ERROR);
-		}
-	}
+        if (errorMsg != null) {
+            setErrorMessage(errorMsg);
+            setStatus(TaskStatus.ERROR);
+        }
+    }
 
-	String prepareData() throws Exception {
-		StringBuilder result = new StringBuilder();
-		result.append("m/z" + "\t" + "retention_time" + "\t" + "p-value" + "\t" + "t-score" + "\t" + "custom_id");
-		result.append("\n");
-		for (PeakListRow pr : this.peakListRows) {
-			if (pr.getPeakInformation().getAllProperties().containsKey("SIGNIFICANCE")) {
-				result.append(pr.getAverageMZ()).append("\t").append(pr.getAverageRT()).append("\t")
-						.append(pr.getPeakInformation().getAllProperties().get(P_VALUE_KEY)).append("\t")
-						.append(pr.getPeakInformation().getAllProperties().get(T_VALUE_KEY)).append("\t")
-						.append("randomText");
-				result.append("\n");
-			} else {
-				throw new Exception("P Value property not present in one or many records during Mummichog calculation");
-			}
-		}
-		return result.toString();
-	}
+    String prepareData() throws Exception {
+        StringBuilder result = new StringBuilder();
+        result.append("m/z" + "\t" + "retention_time" + "\t" + "p-value" + "\t" + "t-score" + "\t" + "custom_id");
+        result.append("\n");
+        for (PeakListRow pr : this.peakListRows) {
+            Map<String, String> properties = pr.getPeakInformation().getAllProperties();
+            if (properties.containsKey(P_VALUE_KEY) && properties.containsKey(T_VALUE_KEY)) {
+                result.append(pr.getAverageMZ()).append("\t").append(pr.getAverageRT()).append("\t")
+                        .append(pr.getPeakInformation().getAllProperties().get(P_VALUE_KEY)).append("\t")
+                        .append(pr.getPeakInformation().getAllProperties().get(T_VALUE_KEY)).append("\t")
+                        .append("randomText");
+                result.append("\n");
+            }
+//            if (pr.getPeakInformation().getAllProperties().containsKey("SIGNIFICANCE")) {
+//                result.append(pr.getAverageMZ()).append("\t").append(pr.getAverageRT()).append("\t")
+//                        .append(pr.getPeakInformation().getAllProperties().get(P_VALUE_KEY)).append("\t")
+//                        .append(pr.getPeakInformation().getAllProperties().get(T_VALUE_KEY)).append("\t")
+//                        .append("randomText");
+//                result.append("\n");
+//            } else {
+//                throw new Exception("P Value property not present in one or many records during Mummichog calculation");
+//            }
+        }
+        return result.toString();
+    }
 }
